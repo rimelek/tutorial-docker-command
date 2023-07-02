ea64add0600c3431cf1836e1ee130159d880395b
Ákos Takács <takacs.akos@it-sziget.hu>
2023-07-02 09:59:04 +0200

    exit when error happens during listing containers

--- list.sh
+++ list.sh
@@ -3,7 +3,9 @@
 source ./resources.sh
 
 echo "$(
+  set -eu -o pipefail
   echo -e "\e[37mContainer\e[0m||\e[37mCommand\e[0m||\e[37mOutput\e[0m"
+  index=0
   while (( index < ${#TESTS[@]} )); do
     version="${TESTS[$index]}"; (( index+=2 ))
 
