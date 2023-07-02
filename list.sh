540fef5a8f5493b08b0ed0e9c9630414b2832fa4
Ákos Takács <takacs.akos@it-sziget.hu>
2023-07-02 02:19:05 +0200

    fix incrementing index when listing containers

--- list.sh
+++ list.sh
@@ -5,7 +5,7 @@ source ./resources.sh
 echo "$(
   echo -e "\e[37mContainer\e[0m||\e[37mCommand\e[0m||\e[37mOutput\e[0m"
   while (( index < ${#TESTS[@]} )); do
-    version="${TESTS[$index]}"; (( ++index ))
+    version="${TESTS[$index]}"; (( index+=2 ))
 
     container_inspect "$CONTAINER_NAME_PREFIX-$version-0"
     container_inspect "$CONTAINER_NAME_PREFIX-$version-1"
